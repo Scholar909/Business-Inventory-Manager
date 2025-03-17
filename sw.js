c49b13f1f1a6fe052c62917a45d40b882873bea2
Scholar909 <enehogbole909@gmail.com>
2025-03-17 16:02:57 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,7 +13,8 @@ const urlsToCache = [
     "/jottings.js",
     "/paid.js",
     "/products.js",
-    "/icon.png"
+    "/icon-192x192.png",
+    "/icon-512x512.png"
 ];
 
 self.addEventListener("install", (event) => {
